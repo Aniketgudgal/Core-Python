92095200e46cc42b53b4e70eea912e38af139fc9
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-10-03 22:58:29 +0530

    basic data type in python

---
+++ Operators/dataType.py
@@ -0,0 +1,22 @@
+#Q2. Write a python program to input all basic data types and print its output.
+
+a = float(input("Enter the float value: "))
+b = bool(input("Enter the boolean value true or false: "))
+c = int(input("Enter the integer value: "))
+d = str(input("Enter the string: "))
+
+print("\t")
+print("a variable contain value: ",a)
+print("type of a ",type(a))
+
+print("\t")
+print("b variable contain value: ",b)
+print("type of a ",type(b))
+
+print("\t")
+print("c variable contain value: ",c)
+print("type of a ",type(c))
+
+print("\t")
+print("d variable contain value: ",d)
+print("type of a ",type(d))
