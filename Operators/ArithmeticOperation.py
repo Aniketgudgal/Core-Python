358a266b8165b73b4c72e0e1acccf6d2406bf0bb
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-10-05 12:30:36 +0530

    All Arithmetic operation

---
+++ Operators/ArithmeticOperation.py
@@ -0,0 +1,26 @@
+'''Q 3. Write a java program to enter two numbers and perform all arithmetic operations. '''
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = a+b;
+d = a - b;
+e = a * b;
+f = a / b;
+g = a // b; # division without fraction
+h = a % b;
+# print("Addition of two number is: "(a+b))
+print("Addition of two number is: ",c)
+
+# print("Substration of two number is: "(a-b))
+print("Substration of two number is: ",d)
+
+# print("Multiplication of two number is: "(a*b))
+print("Multiplication of two number is: ",e)
+
+#print("Divison of two number is: "(a/b))
+print("division of two number is: ",f)
+
+# print("division without fraction is: "(a//b))
+print("Divison without fraction is: ",g)
+
+# print("Remander is: "(a%b))
+print("Remander is: ",h)
