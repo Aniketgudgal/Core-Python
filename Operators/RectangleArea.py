0b2ef2dbb0e4194ceb82cfdd24e678a23fc290b7
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-10-05 12:33:39 +0530

    Area of Rectangle using python

---
+++ Operators/RectangleArea.py
@@ -0,0 +1,9 @@
+'''Q 4. Write a java program to enter length and breadth of a rectangle and find its area.
+Formula - area= length * breadth; '''
+
+length = int(input("Enter the length of rectangle: "))
+breadth = int(input("Enter the breadth of rectangle: "))
+
+area = length * breadth;
+
+print("Area of Rectangle is: ",area);
