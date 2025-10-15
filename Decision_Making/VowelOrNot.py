fd4915e78a3a92fbcd2c7718a1ea9b9cf38989dd
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-10-15 18:53:13 +0530

    check Alphabet it is Vowel or Consonant

---
+++ Decision_Making/VowelOrNot.py
@@ -0,0 +1,10 @@
+#Q9. Write a Python program to input any alphabet and check whether it is vowel or consonant.
+
+ch = input("Enter the character: ")
+
+if ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U':
+	print("The Alphabet is vowel")
+elif ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+	print("The Alphabet is vowel")
+else:
+	print("The Alphabet is consonant")
