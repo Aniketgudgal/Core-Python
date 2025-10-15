8ddc3c4be3540572bc946976a0a07f87c4eecfbd
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-10-15 18:48:07 +0530

    Check Whether a year is a leap year or not

---
+++ Decision_Making/CheckLeapYearOrNot.py
@@ -0,0 +1,8 @@
+#Q8. Write a Python program to check whether a year is a leap year or not.
+
+year = int(input("Enter the year: "))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+	print("Leap Year")
+else:
+	print("Not a Leap Year")
