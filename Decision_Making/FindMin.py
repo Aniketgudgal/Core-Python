4b3f0171bb72ef2d577aeb7e8c1de10f4124fb59
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-10-16 18:26:12 +0530

    Find Minimum between Two Numbers

---
+++ Decision_Making/FindMin.py
@@ -0,0 +1,9 @@
+# Q15. Write a Python program to find the minimum between two numbers.
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 > num2:
+	print("Second Number is minimum")
+else:
+	print("First Number is minimum")
