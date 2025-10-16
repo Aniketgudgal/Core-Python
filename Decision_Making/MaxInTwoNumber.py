9236d3e425608afd54e8664bfa6540a9196791b8
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-10-16 18:34:09 +0530

    Find Maximum Between Two Numbers

---
+++ Decision_Making/MaxInTwoNumber.py
@@ -0,0 +1,9 @@
+#Q17. Write a Python program to find the maximum between two numbers.
+
+num1 = int(input("Enter the First Number: "))
+num2 = int(input("Enter the Second Number: "))
+
+if num1 > num2:
+	print("First number is maximum")
+else:
+	print("Second number is maximum")
