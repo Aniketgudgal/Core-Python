1a78ec90f28b72c3dbdafad68036e32be4c5c5eb
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-10-16 18:36:26 +0530

    Check Student is Pass or Fail

---
+++ Decision_Making/PassOrFail.py
@@ -0,0 +1,8 @@
+#Q18. Given a student’s score, print Pass if it’s 40 or above, otherwise print Fail.
+
+score = int(input("Enter the score of student: "))
+
+if score >= 40:
+	print("Student is Pass")
+else:
+	print("Student is Fail")
